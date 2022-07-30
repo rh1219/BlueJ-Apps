691bb78ffdff0d23e6c0804f98e9472a33945c05
rh1219 <91318841+rh1219@users.noreply.github.com>
2022-07-30 14:02:49 +0100

    f

--- app214-stock/InputReader.java
+++ app214-stock/InputReader.java
@@ -31,43 +31,43 @@ public class InputReader
     {
         String inputLine = null;
         boolean isValid = false;
-        
+
         while(!isValid)
         {
             System.out.print(prompt);         // print prompt
             inputLine = reader.nextLine();
-            
+
             if(!inputLine.isEmpty())
                 isValid = true;
-            else 
+            else
                 System.out.println("\nYour input is blank!\n");
         }
 
         return inputLine;
     }
-    
+
     /**
      * Read a the next int from standard imput (the text terminal),
      * and return it as an interger.
-     * 
+     *
      * @return Integer typed by user.
      */
     public int getInt(String prompt)
     {
         int number = 0;
         boolean isValid = false;
-        
+
         while(!isValid)
         {
             System.out.println(prompt);         // print prompt
             number = reader.nextInt();
-            
+
             if(number > 0)
             {
                 isValid = true;
             }
-            else 
-                System.out.println("Your value is less than zero!");
+            else
+                System.out.println("Your value is less than zero");
         }
 
         return number;
